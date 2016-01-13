40343aff8195069574354c57de407dac387d652e
Jan Suchotzki <jan@lernmoment.de>
2016-01-13 10:30:31 +0100

    Fügt die Division hinzu.

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -9,7 +9,7 @@ namespace Taschenrechner
             // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
             string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
             string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein: ");
-            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation ein (+ oder -): ");
+            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation ein (+, -, /): ");
 
             // Wandel Text in Gleikommazahlen
             // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
@@ -31,6 +31,10 @@ namespace Taschenrechner
                     break;
 
                 case "/":
+                    resultat = Dividiere(ersteZahl, zweiteZahl);
+                    Console.WriteLine("Der Wert des Quotienten ist: {0}", resultat);
+                    break;
+
                 case "*":
                     Console.WriteLine("Diese Operation wird bald unterstützt. Versprochen ;-)!");
                     break;
@@ -64,6 +68,11 @@ namespace Taschenrechner
             double differenz = minuend - subtrahent;
 
             return differenz;
+        }
+
+        static double Dividiere(double dividend, double divisor)
+        {
+            return dividend / divisor;
         }
     }
 }
