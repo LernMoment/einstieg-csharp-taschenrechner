4714e9b3a7a544e31215af7dc6e5e3a052d219ca
Jan Suchotzki <jan@lernmoment.de>
2016-01-30 22:39:39 +0100

    Ersetzt ToDouble mit TryParse => Input Validierung ohne Exceptions

--- Taschenrechner/ConsoleView.cs
+++ Taschenrechner/ConsoleView.cs
@@ -44,11 +44,25 @@ namespace Taschenrechner
 
         private double HoleZahlVomBenutzer()
         {
-            string zahl;
+            string eingabe;
+            double zahl;
             Console.Write("Bitte gib eine Zahl für die Berechnung ein: ");
-            zahl = Console.ReadLine();
+            eingabe = Console.ReadLine();
 
-            return Convert.ToDouble(zahl);
+            while (!double.TryParse(eingabe, out zahl))
+            {
+                Console.WriteLine("Du musst eine gültige Gleitkommazahl eingeben!");
+                Console.WriteLine("Neben den Ziffern 0-9 sind lediglich die folgenden Sonderzeichen erlaubt: ,.-");
+                Console.WriteLine("Dabei muss das - als erstes Zeichen vor einer Ziffer gesetzt werden.");
+                Console.WriteLine("Der . fungiert als Trennzeichen an Tausenderstellen.");
+                Console.WriteLine("Das , ist das Trennzeichen für die Nachkommastellen.");
+                Console.WriteLine("Alle drei Sonderzeichen sind optional!");
+                Console.WriteLine();
+                Console.Write("Bitte gib erneut eine Zahl für die Berechnung ein: ");
+                eingabe = Console.ReadLine();
+            }
+
+            return zahl;
         }
 
         private string HoleOperatorVomBenutzer()
