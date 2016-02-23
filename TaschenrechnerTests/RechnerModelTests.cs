12143c645637f1d9f8059ff4924af748d8621cbc
Jan Suchotzki <jan@lernmoment.de>
2016-02-23 19:19:02 +0100

    Fügt die Datei mit dem eigentlichen Unittest hinzu. Diese fehlte im Commit #a3afad23

---
+++ TaschenrechnerTests/RechnerModelTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taschenrechner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner.Tests
+{
+    [TestClass()]
+    public class RechnerModelTests
+    {
+        [TestMethod()]
+        public void Berechne_DivisionDurchNull_ErgibtUnendlich()
+        {
+            RechnerModel model = new RechnerModel();
+
+            model.Operation = "/";
+            model.ErsteZahl = 10;
+            model.ZweiteZahl = 0;
+
+            model.Berechne();
+
+            Assert.AreEqual(Double.PositiveInfinity, model.Resultat);
+        }
+    }
+}
