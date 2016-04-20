42a40c3a312052b4d9cbd5c9010d20f41a6c11bd
Jan Suchotzki <jan@lernmoment.de>
2016-04-20 14:18:40 +0200

    Program verwendet NLog

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using NLog;
 
 namespace Taschenrechner
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            logger.Info("Taschenrechner gestartet!");
+
             RechnerModel model = new RechnerModel();
             ConsoleView view = new ConsoleView(model);
             AnwendungsController controller = new AnwendungsController(view, model);
 
+            logger.Info("Taschenrechner zusammen gebaut!");
+
             controller.Ausfuehren();
+
+            logger.Info("Taschenrechner beendet");
         }
     }
 }
