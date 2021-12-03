7c7f27bd7444c7b29ceed35aa644f53411a4e84e
MK-NEUKO <m.koch@know-how-akademie.de>
2021-12-03 08:07:04 +0100

    Umbau der Programm.cs im Stile von .net 6.0

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -1,16 +1,7 @@
-﻿using System;
+﻿using Taschenrechner;
 
-namespace Taschenrechner
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            RechnerModel model = new RechnerModel();
-            ConsoleView view = new ConsoleView(model);
-            AnwendungsController controller = new AnwendungsController(view, model);
+RechnerModel model = new RechnerModel();
+ConsoleView view = new ConsoleView(model);
+AnwendungsController controller = new AnwendungsController(view, model);
 
-            controller.Ausfuehren();
-        }
-    }
-}
+controller.Ausfuehren();
